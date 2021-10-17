9724bd6496f56863931a18ca977c695f60d0cf97
Zach <45979377+zawachte-msft@users.noreply.github.com>
2021-10-17 10:11:06 -0400

    fix pipelien (#159)

--- hack/for-pipeline.sh
+++ hack/for-pipeline.sh
@@ -21,7 +21,7 @@ DOCKER_AUTH_B64="$(echo -n "$DOCKER_AUTH" | base64 | tr -d '\n')"
 DOCKER_SECRET="{\"auths\":{\"https:\/\/mocimages.azurecr.io\":{\"username\":\"${DOCKER_USERNAME}\",\"password\":\"${DOCKER_PASSWORD}\",\"auth\":\"${DOCKER_AUTH_B64}\"}}}"
 DOCKER_SECRET_B64="$(echo -n "$DOCKER_SECRET" | base64 | tr -d '\n')"
 
-cat > config/manager/manager_image_patch.yaml <<EOF
+cat > config/default/manager_image_patch.yaml <<EOF
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -43,11 +43,6 @@ kind: Kustomization
 resources:
   - manager.yaml
   - dockercredentials.yaml
-
-patchesStrategicMerge:
-  - manager_pull_policy.yaml
-  - manager_image_patch.yaml
-  - manager_auth_proxy_patch.yaml
 EOF
 
 cat > config/manager/dockercredentials.yaml <<EOF
